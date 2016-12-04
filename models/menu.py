fa9ca536c9bba224d188f2d22174e0750ecf27c7
huishili <huishili95@gmail.com>
2016-12-04 21:42:44 +0000

    changed logo/header

--- models/menu.py
+++ models/menu.py
@@ -5,9 +5,9 @@
 # Customize your APP title, subtitle and menus here
 # ----------------------------------------------------------------------------------------------------------------------
 
-response.logo = A(B('web', SPAN(2), 'py'), XML('&trade;&nbsp;'),
-                  _class="navbar-brand", _href="http://www.web2py.com/",
-                  _id="web2py-logo")
+response.logo = A(B('GameDayCalendar'), XML('&trade;&nbsp;'),
+                  _class="navbar-brand", _href="",
+                  _id="gamedaycalendar")
 response.title = request.application.replace('_', ' ').title()
 response.subtitle = ''
 
@@ -27,10 +27,10 @@ response.google_analytics_id = None
 # ----------------------------------------------------------------------------------------------------------------------
 # this is the main application menu add/remove items as required
 # ----------------------------------------------------------------------------------------------------------------------
-
-response.menu = [
-    (T('Home'), False, URL('default', 'index'), [])
-]
+#
+# response.menu = [
+#     (T('Home'), False, URL('default', 'index'), [])
+# ]
 
 DEVELOPMENT_MENU = True
 
@@ -48,77 +48,77 @@ def _():
     # ------------------------------------------------------------------------------------------------------------------
     # useful links to internal and external resources
     # ------------------------------------------------------------------------------------------------------------------
-    response.menu += [
-        (T('My Sites'), False, URL('admin', 'default', 'site')),
-        (T('This App'), False, '#', [
-            (T('Design'), False, URL('admin', 'default', 'design/%s' % app)),
-            LI(_class="divider"),
-            (T('Controller'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/controllers/%s.py' % (app, ctr))),
-            (T('View'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/views/%s' % (app, response.view))),
-            (T('DB Model'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/models/db.py' % app)),
-            (T('Menu Model'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/models/menu.py' % app)),
-            (T('Config.ini'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/private/appconfig.ini' % app)),
-            (T('Layout'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/views/layout.html' % app)),
-            (T('Stylesheet'), False,
-             URL(
-                 'admin', 'default', 'edit/%s/static/css/web2py-bootstrap3.css' % app)),
-            (T('Database'), False, URL(app, 'appadmin', 'index')),
-            (T('Errors'), False, URL(
-                'admin', 'default', 'errors/' + app)),
-            (T('About'), False, URL(
-                'admin', 'default', 'about/' + app)),
-        ]),
-        (T('Documentation'), False, '#', [
-            (T('Online book'), False, 'http://www.web2py.com/book'),
-            LI(_class="divider"),
-            (T('Preface'), False,
-             'http://www.web2py.com/book/default/chapter/00'),
-            (T('Introduction'), False,
-             'http://www.web2py.com/book/default/chapter/01'),
-            (T('Python'), False,
-             'http://www.web2py.com/book/default/chapter/02'),
-            (T('Overview'), False,
-             'http://www.web2py.com/book/default/chapter/03'),
-            (T('The Core'), False,
-             'http://www.web2py.com/book/default/chapter/04'),
-            (T('The Views'), False,
-             'http://www.web2py.com/book/default/chapter/05'),
-            (T('Database'), False,
-             'http://www.web2py.com/book/default/chapter/06'),
-            (T('Forms and Validators'), False,
-             'http://www.web2py.com/book/default/chapter/07'),
-            (T('Email and SMS'), False,
-             'http://www.web2py.com/book/default/chapter/08'),
-            (T('Access Control'), False,
-             'http://www.web2py.com/book/default/chapter/09'),
-            (T('Services'), False,
-             'http://www.web2py.com/book/default/chapter/10'),
-            (T('Ajax Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/11'),
-            (T('Components and Plugins'), False,
-             'http://www.web2py.com/book/default/chapter/12'),
-            (T('Deployment Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/13'),
-            (T('Other Recipes'), False,
-             'http://www.web2py.com/book/default/chapter/14'),
-            (T('Helping web2py'), False,
-             'http://www.web2py.com/book/default/chapter/15'),
-            (T("Buy web2py's book"), False,
-             'http://stores.lulu.com/web2py'),
-        ]),
-    ]
+    # response.menu += [
+    #     (T('My Sites'), False, URL('admin', 'default', 'site')),
+    #     (T('This App'), False, '#', [
+    #         (T('Design'), False, URL('admin', 'default', 'design/%s' % app)),
+    #         LI(_class="divider"),
+    #         (T('Controller'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/controllers/%s.py' % (app, ctr))),
+    #         (T('View'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/views/%s' % (app, response.view))),
+    #         (T('DB Model'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/models/db.py' % app)),
+    #         (T('Menu Model'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/models/menu.py' % app)),
+    #         (T('Config.ini'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/private/appconfig.ini' % app)),
+    #         (T('Layout'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/views/layout.html' % app)),
+    #         (T('Stylesheet'), False,
+    #          URL(
+    #              'admin', 'default', 'edit/%s/static/css/web2py-bootstrap3.css' % app)),
+    #         (T('Database'), False, URL(app, 'appadmin', 'index')),
+    #         (T('Errors'), False, URL(
+    #             'admin', 'default', 'errors/' + app)),
+    #         (T('About'), False, URL(
+    #             'admin', 'default', 'about/' + app)),
+    #     ]),
+    #     (T('Documentation'), False, '#', [
+    #         (T('Online book'), False, 'http://www.web2py.com/book'),
+    #         LI(_class="divider"),
+    #         (T('Preface'), False,
+    #          'http://www.web2py.com/book/default/chapter/00'),
+    #         (T('Introduction'), False,
+    #          'http://www.web2py.com/book/default/chapter/01'),
+    #         (T('Python'), False,
+    #          'http://www.web2py.com/book/default/chapter/02'),
+    #         (T('Overview'), False,
+    #          'http://www.web2py.com/book/default/chapter/03'),
+    #         (T('The Core'), False,
+    #          'http://www.web2py.com/book/default/chapter/04'),
+    #         (T('The Views'), False,
+    #          'http://www.web2py.com/book/default/chapter/05'),
+    #         (T('Database'), False,
+    #          'http://www.web2py.com/book/default/chapter/06'),
+    #         (T('Forms and Validators'), False,
+    #          'http://www.web2py.com/book/default/chapter/07'),
+    #         (T('Email and SMS'), False,
+    #          'http://www.web2py.com/book/default/chapter/08'),
+    #         (T('Access Control'), False,
+    #          'http://www.web2py.com/book/default/chapter/09'),
+    #         (T('Services'), False,
+    #          'http://www.web2py.com/book/default/chapter/10'),
+    #         (T('Ajax Recipes'), False,
+    #          'http://www.web2py.com/book/default/chapter/11'),
+    #         (T('Components and Plugins'), False,
+    #          'http://www.web2py.com/book/default/chapter/12'),
+    #         (T('Deployment Recipes'), False,
+    #          'http://www.web2py.com/book/default/chapter/13'),
+    #         (T('Other Recipes'), False,
+    #          'http://www.web2py.com/book/default/chapter/14'),
+    #         (T('Helping web2py'), False,
+    #          'http://www.web2py.com/book/default/chapter/15'),
+    #         (T("Buy web2py's book"), False,
+    #          'http://stores.lulu.com/web2py'),
+    #     ]),
+    #]
 
 
 if DEVELOPMENT_MENU:
